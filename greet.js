25b36e8d80129e7fd08fc6f3aa1d8b204156c53d
Amirah <amirahdavids2503@gmail.com>
2020-09-01 00:50:19 +0200

    added new functions

--- greet.js
+++ greet.js
@@ -1,8 +1,11 @@
-module.exports=function GreetFactory(stored) {
+module.exports = function GreetFactory() {
 
-    var userMap = stored || {};
+    var userMap = { };
 
     function greetUser(name, language) {
+        if (!language || !name) {
+            return "";
+        }
         addUser(name);
         switch (language) {
             case "english":
@@ -20,6 +23,7 @@ module.exports=function GreetFactory(stored) {
         if (userMap[userName] === undefined) {
             userMap[userName] = 0;
         }
+        userMap[userName]++
     }
 
     function getGreetCount() {
@@ -36,54 +40,46 @@ module.exports=function GreetFactory(stored) {
         return "";
     }
 
-    function getAllUsers(){
-        // this is to be used for local storage
+    function getAllUsers() {
         return userMap;
     }
 
-    function resetBtn(){
-        userMap = {};
-        localStorage.clear("greetedUsers");
+    function getAllUsersAsList() {
+        return Object.keys(userMap);
     }
 
+    function getGreetCountForUser(name) {
+        if (userMap[name] !== undefined) {
+            return userMap[name];
+        }
+        return 0;
+    }
+
+    function resetBtn() {
+        userMap = {};
+    }
+
+    function displayFlashMsg(name, language) {
+        if (name === "" && language === undefined) {
+            return "please enter a name and language"
+        }
+        if (name === "") {
+            return "no name entered"
+        }
+        if (language === undefined) {
+            return "no language selected"
+        }
+    };
 
     return {
         greetUser,
+        addUser,
         getGreetCount,
         getNameFromInput,
         getAllUsers,
-        resetBtn
+        resetBtn,
+        getAllUsersAsList,
+        getGreetCountForUser,
+        displayFlashMsg
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
