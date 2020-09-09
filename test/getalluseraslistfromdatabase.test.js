af729b3dca5adf2024b92d0e173f2b125efeb13b
Amirah <amirahdavids2503@gmail.com>
2020-09-09 18:47:14 +0200

    added test for getting all users as list from db

---
+++ test/getalluseraslistfromdatabase.test.js
@@ -0,0 +1,44 @@
+const assert = require("assert");
+const GreetFactory = require('../greet')
+
+
+describe("The get all users as list function", function () {
+
+	const pg = require("pg");
+	const Pool = pg.Pool;
+	const connectionString = process.env.DATABASE_URL || 'postgresql://amirah:coder123@localhost:5432/greet_db_test';
+	const pool = new Pool({
+		connectionString
+	});
+
+	const greetFactory = GreetFactory(pool);
+
+	const INSERT_QUERY = " into person (first_name, counter) values ($1, $2)";
+
+	beforeEach(async function () {
+		await pool.query("delete from person");
+	});
+
+	it("should display all users in a list ", async function () {
+
+        await greetFactory.addUserToDatabase("Mike");
+        await greetFactory.addUserToDatabase("Spike");
+		
+		const results = await greetFactory.getAllUsersAsListFromDatabase();
+
+		
+		assert.deepEqual([{
+            first_name: 'Mike'
+          },
+          {
+            first_name: 'Spike'
+          }], results);
+
+	});
+
+	
+	after(function () {
+		pool.end();
+	})
+
+});
