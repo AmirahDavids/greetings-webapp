d550122be31db3b923ab46031de4d84b8a7dd288
Amirah <amirahdavids2503@gmail.com>
2020-09-10 08:49:02 +0200

    testing queries

--- test/ getgreetcountfromdatabase.test.js
+++ test/ getgreetcountfromdatabase.test.js
@@ -5,7 +5,7 @@ describe("The getGreetCountForUserFromDatabase function", function () {
 
     const pg = require("pg");
     const Pool = pg.Pool;
-    const connectionString = process.env.DATABASE_URL || 'postgresql://amirah:coder123@localhost:5432/greet_db_test';
+    const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/greet_db_test';
     const pool = new Pool({
         connectionString
     });
--- test/getalluseraslistfromdatabase.test.js
+++ test/getalluseraslistfromdatabase.test.js
@@ -6,7 +6,7 @@ describe("The get all users as list function", function () {
 
 	const pg = require("pg");
 	const Pool = pg.Pool;
-	const connectionString = process.env.DATABASE_URL || 'postgresql://amirah:coder123@localhost:5432/greet_db_test';
+	const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/greet_db_test';
 	const pool = new Pool({
 		connectionString
 	});
